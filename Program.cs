00194ff784609affc8fbe6a511340b04829f6fc2
jasperdun <jasperdun@DESKTOP-7LK7SGO>
2025-03-26 13:41:26 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VideoStoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoStore = new VideoStore();
+            videoStore.AddMovie("Dune");
+            videoStore.AddMovie("No Time to Die");
+            videoStore.AddMovie("Spider-Man: No Way Home");
+
+            videoStore.ViewMovie("Dune");
+            videoStore.ViewMovie("Dune");
+            videoStore.ViewMovie("Spider-Man: No Way Home");
+
+            videoStore.DisplayViewCounts();
+        }
+    }
+
+    public class VideoStore
+    {
+        private Dictionary<string, int> movieViewCounts;
+
+        public VideoStore()
+        {
+            movieViewCounts = new Dictionary<string, int>();
+        }
+
+        public void AddMovie(string movieName)
+        {
+            if (!movieViewCounts.ContainsKey(movieName))
+            {
+                movieViewCounts[movieName] = 0;
+            }
+        }
+
+        public void ViewMovie(string movieName)
+        {
+            if (movieViewCounts.ContainsKey(movieName))
+            {
+                movieViewCounts[movieName]++;
+            }
+            else
+            {
+                Console.WriteLine($"Movie '{movieName}' not found in the store.");
+            }
+        }
+
+        public void DisplayViewCounts()
+        {
+            Console.WriteLine("Movie View Counts:");
+            foreach (var movie in movieViewCounts)
+            {
+                Console.WriteLine($"{movie.Key}: {movie.Value} views");
+            }
+        }
+    }
+}
